71256f486428a67d2948dd8cdb83413779351777
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-09-22 12:16:55 +0300

    r
    r

--- src/index.js
+++ src/index.js
@@ -1,7 +1,82 @@
-import axios from 'axios';
+// const axios = require('axios').default;
+import './css/styles.css';
+import { searchQuery } from './js/searchQuery';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-axios
-  .get(
-    'https://pixabay.com/api/?key=30029348-12068a2fdca19007a6804d89e&q=yellow+flowers&image_type=photo'
-  )
-  .then(console.log);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  docClose: true,
+});
+
+const newPop = new searchQuery();
+
+const refs = {
+  form: document.querySelector('#search-form'),
+};
+const inputEl = refs.form.querySelector('input[type=text]');
+const btnEl = refs.form.querySelector('button[type=submit]');
+const gallery = document.querySelector('.gallery');
+
+refs.form.addEventListener('submit', onSearchInput);
+
+async function onSearchInput(e) {
+  e.preventDefault();
+
+  newPop.query = e.currentTarget.elements.searchQuery.value.trim();
+  newRequest();
+}
+
+async function zopa() {
+  const data = await newPop.newRequest();
+  // console.log(data);
+  initializeName(data.hits);
+  // console.log(data.hits);
+}
+
+function initializeName(resHits) {
+  const privet = resHits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="gallery__item post" style='overflow: hidden; position: relative'>
+           <a href="${largeImageURL}">
+            <img
+              class="gallery__image"
+              src="${webformatURL}"
+              alt="${tags}"
+              loading="lazy"
+
+            />
+            </a>
+            <div class="info">
+                <p class="info-item" style='display: flex; flex-direction: column'>
+                    <b>Likes</b><span>${likes}</span>
+                </p>
+                <p class="info-item" style='display: flex; flex-direction: column'>
+                    <b>Views</b><span>${views}</span>
+                </p>
+                <p class="info-item" style='display: flex; flex-direction: column'>
+                    <b>Comments</b><span>${comments}</span>
+                </p>
+                <p class="info-item" style='display: flex; flex-direction: column'>
+                    <b>Downloads</b><span>${downloads}</span>
+                </p>
+            </div>
+        </div>`;
+      }
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', privet);
+}
---
+++ src/js/searchQuery.js
@@ -0,0 +1,41 @@
+const axios = require('axios').default;
+
+export class searchQuery {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  async request() {
+    const params = {
+      url: 'https://pixabay.com/api/',
+      key: '30029348-12068a2fdca19007a6804d89e',
+      q: this.query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page: this.page,
+    };
+    try {
+      const rest = await axios.get(params.url, { params });
+      const data = await rest.data;
+      console.log(data);
+      return data;
+    } catch (error) {
+      error = new Error().stack;
+    }
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+  set query(value) {
+    this.searchQuery = value;
+  }
+  updatePage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+}
